424edd1f25a454eb05ccfbc2a5a30a4279ed6a1a
Owen Adams <owenadams.main@gmail.com>
2022-05-24 02:12:23 +0000

    add functionality to keydown event listener to not increase speed when space is tapped. ready for review. #2

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const f1Data = {
     y: 0,
   }
 }
+let intervalID = null;
 
 function startF1Racer() {
     let leftPos = f1Racer.offsetLeft;
@@ -14,6 +15,12 @@ function startF1Racer() {
 
 window.addEventListener('keydown', e => {
   if (e.keyCode === 32) {
-    const intervalID = setInterval(startF1Racer, 16);
+    if (intervalID !== null) {
+      clearInterval(intervalID)
+      intervalID = setInterval(startF1Racer, 16);
+    } else if (intervalID === null) {
+      intervalID = setInterval(startF1Racer, 16);
+    }
   };
+  console.log('space was tapped');
 });
