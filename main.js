42c9f94d139576053c78d5035b38c930e97e2e7a
Owen Adams <owenadams.main@gmail.com>
2022-05-25 02:47:06 +0000

    add functionality to the startF1Racer function to steer the vehicle in the direction of which the vehicle is facing N, S, E, or W. Ready for review. #3

--- main.js
+++ main.js
@@ -9,24 +9,40 @@ const f1Data = {
 let intervalID = null;
 
 function startF1Racer() {
-    let leftPos = f1Racer.offsetLeft;
-    f1Racer.style.left = leftPos + 3 + 'px';
+  let topPos = f1Racer.offsetTop;
+  let leftPos = f1Racer.offsetLeft;
+  if (f1Data.direction === 'north') {
+    topPos -= 3;
+    f1Racer.style.top = topPos + 'px';
+    f1Data.location.y = f1Racer.style.top;
+  } else if (f1Data.direction === 'east') {
+    leftPos += 3;
+    f1Racer.style.left = leftPos + 'px';
     f1Data.location.x = f1Racer.style.left;
+  } else if (f1Data.direction === 'south') {
+    topPos += 3;
+    f1Racer.style.top = topPos + 'px';
+    f1Data.location.y = f1Racer.style.top;
+  } else if (f1Data.direction === 'west') {
+    leftPos -= 3;
+    f1Racer.style.left = leftPos + 'px';
+    f1Data.location.x = f1Racer.style.left;
+  }
 }
 
 function f1RacerControl(e) {
-  if (e.keyCode === 37 || e.keyCode === 65) {
-    f1Data.direction = 'west';
-    f1Racer.style.transform = 'rotate(180deg)'
-  } else if (e.keyCode === 38 || e.keyCode === 87) {
+  if (e.keyCode === 38 || e.keyCode === 87) {
     f1Data.direction = 'north';
     f1Racer.style.transform = 'rotate(-90deg)'
-  } else if (e.keyCode === 39 || e.keyCode === 68) {
-    f1Data.direction = 'east';
-    f1Racer.style.transform = 'rotate(0deg)'
+  } else if (e.keyCode === 37 || e.keyCode === 65) {
+    f1Data.direction = 'west';
+    f1Racer.style.transform = 'rotate(180deg)'
   } else if (e.keyCode === 40 || e.keyCode === 83) {
     f1Data.direction = 'south';
     f1Racer.style.transform = 'rotate(90deg)'
+  } else if (e.keyCode === 39 || e.keyCode === 68) {
+    f1Data.direction = 'east';
+    f1Racer.style.transform = 'rotate(0deg)'
   } else if (e.keyCode === 32) {
     startF1Racer()
     if (intervalID !== null) {
@@ -36,7 +52,6 @@ function f1RacerControl(e) {
       intervalID = setInterval(startF1Racer, 16);
     }
   }
-  console.log('value of f1Data.direction', f1Data.direction);
 };
 
 window.addEventListener('keydown', f1RacerControl)
